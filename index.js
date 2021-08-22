885f997c444b0ace3a1d751cdb527b142a0c1172
Nick Zabkar <nzabkar51@gmail.com>
2021-08-22 14:45:31 -0400

    Changed questions to arrow func

--- index.js
+++ index.js
@@ -5,59 +5,61 @@ const util = require('util');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: "input",
-        name:"title",
-        message:"What is your project title?"
-    },
-    {
-        type: "input",
-        name:"description",
-        message:"Enter a description of your project"
-    },
-    {
-        type: "input",
-        name:"installation",
-        message:"Add installation instructions for your project"
-    },
-    {
-        type: "input",
-        name:"usage",
-        message:"Add usage information for your project"
-    },
-    {
-        type: "input",
-        name:"contribution",
-        message:"Tell others how to contribute to your project"
-    },
-    {
-        type: "input",
-        name:"tests",
-        message:"Add test instructions for your project"
-    },
-    {
-        type: "list",
-        name:"liscense",
-        message:"What liscense are you using?",
-        choices:[
-            "Apache 2.0",
-            "GPL 2.0",
-            "GPL 3.0",
-            "MIT"
-        ],
-    },
-    {
-        type: "input",
-        name:"github",
-        message:"Enter your GitHub username"
-    },
-    {
-        type: "input",
-        name:"email",
-        message:"Enter your email address"
-    }
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name:"title",
+            message:"What is your project title?"
+        },
+        {
+            type: "input",
+            name:"description",
+            message:"Enter a description of your project"
+        },
+        {
+            type: "input",
+            name:"installation",
+            message:"Add installation instructions for your project"
+        },
+        {
+            type: "input",
+            name:"usage",
+            message:"Add usage information for your project"
+        },
+        {
+            type: "input",
+            name:"contribution",
+            message:"Tell others how to contribute to your project"
+        },
+        {
+            type: "input",
+            name:"tests",
+            message:"Add test instructions for your project"
+        },
+        {
+            type: "list",
+            name:"liscense",
+            message:"What liscense are you using?",
+            choices:[
+                "Apache 2.0",
+                "GPL 2.0",
+                "GPL 3.0",
+                "MIT"
+            ],
+        },
+        {
+            type: "input",
+            name:"github",
+            message:"Enter your GitHub username"
+        },
+        {
+            type: "input",
+            name:"email",
+            message:"Enter your email address"
+        }
+    ]);
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -66,7 +68,7 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions);
+    questions();
 }
 
 // Function call to initialize app
