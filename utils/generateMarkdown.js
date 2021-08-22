2d9f9ef5f299b86a7d1f68440e3b875652e779de
Nick Zabkar <nzabkar51@gmail.com>
2021-08-22 19:36:50 -0400

    Added license section for Apache 2.0

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -28,7 +28,29 @@ const renderLicenseLink = (license) => {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (license) => {
+    if (license === 'Apache 2.0') {
+        return `   Copyright [yyyy] [name of copyright owner]
+
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+     
+          http://www.apache.org/licenses/LICENSE-2.0
+     
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.`;
+    } else if (license === 'GPL v2') {
+        return 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
+    } else if (license === 'GPL v3') {
+        return 'https://www.gnu.org/licenses/gpl-3.0';
+    } else if (license === 'MIT') {
+        return 'https://opensource.org/licenses/MIT';
+    }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -53,7 +75,9 @@ function generateMarkdown(data) {
   ## Tests
   ${data.tests}
   ## Liscense
-  Link: ${renderLicenseLink(data.license)}
+  * Link: ${renderLicenseLink(data.license)}  
+  
+  ${renderLicenseSection(data.license)}
   ## Questions
   * ${data.github}
   * ${data.email}
